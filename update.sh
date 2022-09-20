8793a061257b55abc53795265bae195145304126
Norman Breau <norman@nbsolutions.ca>
2022-09-20 15:19:44 -0300

    update SQLite binaries for getInt/getDouble JNI null support

---
+++ update.sh
@@ -0,0 +1,6 @@
+
+# Update script, to copy the release build of SQLite from the sqlite repository.
+# Assumes the SQLite repo is checked out as a sibling to this repository.
+
+cp ../sqlite/bin/android/sqlite3.aar ./bin/sqlite3.aar
+
