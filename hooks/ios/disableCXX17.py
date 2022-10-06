61c0d3a4f108d0507f8a13ff27c6e77fc7a8549a
Tyler Breau <tylerbreau@hotmail.com>
2022-10-06 10:47:27 -0300

    Fixed xcframework installation. Added hooks to ensure clang cxx language standard is c++17. This is required for XCode to properly build with the binaries.

---
+++ hooks/ios/disableCXX17.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import importlib.util
+import os
+try:
+  from pbxproj import XcodeProject
+  from pbxproj.pbxextensions.ProjectFiles import FileOptions
+except:
+  if input("pbxproj module not found. Can I install the module with \"sudo pip3 install pbxproj\"? [y/any other character] ") == "y":
+    os.system('sudo pip3 install pbxproj')
+    from pbxproj import XcodeProject
+    from pbxproj.pbxextensions.ProjectFiles import FileOptions
+  else:
+    raise ModuleNotFoundError("pbxproj module not found and request to install module was denied.")
+
+import sys
+import shutil
+import xml.etree.ElementTree as ET
+tree = ET.parse(sys.argv[1] + '/config.xml')
+root = tree.getroot()
+name = root.find('{http://www.w3.org/ns/widgets}name').text
+
+project = XcodeProject.load(sys.argv[1] + '/platforms/ios/' + name + '.xcodeproj/project.pbxproj')
+project.remove_flags("CLANG_CXX_LANGUAGE_STANDARD", "c++17")
+project.save()
