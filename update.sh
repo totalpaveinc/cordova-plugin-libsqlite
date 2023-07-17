7ccc217b5dd19d0f71d8174924246841288f7a42
Norman Breau <norman@nbsolutions.ca>
2023-07-17 15:36:12 -0300

    update sqlite binaries

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 # Update script, to copy the release build of SQLite from the sqlite repository.
 # Assumes the SQLite repo is checked out as a sibling to this repository.
 
-cp ../sqlite/bin/android/sqlite3.aar ./bin/sqlite3.aar
+cp ../sqlite/bin/dist/android/sqlite3-release.aar ./bin/sqlite3.aar
 rm -rf ./bin/sqlite3.xcframework
-cp -rf ../sqlite/bin/ios/sqlite3.xcframework ./bin/sqlite3.xcframework
+cp -rf ../sqlite/bin/dist/ios/sqlite3.xcframework ./bin/sqlite3.xcframework
